9886691b76225643ca241f2abe783cf25be6ff82
Graeme Ong <graemeong@192-168-1-3.tpgi.com.au>
2017-01-22 20:00:42 +1100

    [GraemeO] Update bootstrap to include Git installation

--- bootstrap.sh
+++ bootstrap.sh
@@ -4,10 +4,11 @@ apt-get update
 apt-get install -y -q software-properties-common python-software-properties
 add-apt-repository ppa:webupd8team/java
 apt-get update
+
 echo debconf shared/accepted-oracle-license-v1-1 select true | sudo debconf-set-selections
 echo debconf shared/accepted-oracle-license-v1-1 seen true | sudo debconf-set-selections
 apt-get install -y -q oracle-java8-installer
-
 java -version
 
-
+apt-get install -y -q git
+git --version
